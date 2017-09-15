9f4247ebb6d1c36b50ecd4e5fdbb2c1bf979d869
hjjdazzling <127511968@qq.com>
2017-09-15 11:14:30 +0800

    1

---
+++ Hello.java
@@ -0,0 +1,14 @@
+/*************************************************************************
+	> File Name: Hello.java
+	> Author: 
+	> Mail: 
+	> Created Time: 2017年09月15日 星期五 11时11分56秒
+ ************************************************************************/
+
+public class Hello
+{
+    public static void main(String[] args)
+    {
+        System.out.println("hello");
+    }
+}
