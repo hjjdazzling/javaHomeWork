86729a8c023dfb09867b449b8de0b2ac6595f84a
hjjdazzling <1275119618@qq.com>
2017-09-21 20:42:52 +0800

    简单的文件传输

--- TCPClient.java
+++ TCPClient.java
@@ -1,28 +1,81 @@
-package TCP;
+package TCPDocument;
+
 import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
+
 public class TCPClient {
 
+	private Socket client;
+	private DataOutputStream dos;
+	private FileInputStream input;
 
-	public static void main(String[] args) throws Exception{
-		// TODO Auto-generated method stub
-		Scanner in = new Scanner(System.in);
-		System.out.print("请输入要连接的端口号:");
-		int number = in.nextInt();
-		Socket client = new Socket("localhost",number);
-		DataOutputStream dos = new DataOutputStream(client.getOutputStream());
-		String str = "";
-		while(str != "再见")
-		{
-		System.out.println("请输入要发送的信息");
-		Scanner input = new Scanner(System.in);
-		str = input.nextLine();
-		dos.writeUTF(str);
+	public static void main(String[] args) {
+		new TCPClient().connect();
+	}
+	
+	public void connect() {
+		try {
+			client = new Socket("localhost", 8888);
+			dos = new DataOutputStream(client.getOutputStream());
+			send();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		client.close();
+	}
+
+	public String Document() {	
+		 Scanner in = new Scanner(System.in); System.out.print("请输入文件路径:"); 
+		 String pathName = in.nextLine();
+		 return pathName;
+	}
+
+	public void send() {
+		try {
+			String pathName = Document();
+			File file = new File(pathName);
+			int i = 1;
+
+			if (file.exists()) {
+				input = new FileInputStream(file);
+				do {
+					byte[] data = new byte[1024];
+					i = input.read(data);
+					String str = new String(data);
+					dos.writeUTF(str);
+					//System.out.println(str);
+					dos.flush();
+				} while (i > 0);
+			} else {
+				System.out.println("目标文件不存在");
+				send();
+			}
+			System.out.println("传输完毕");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+	}
+
+	public void disconnect() {
+		try {
+			if (dos != null) {
+				dos.close();
+			}
+			if (client != null) {
+				client.close();
+			}
+			if (input != null) {
+				input.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
--- TCPServer.java
+++ TCPServer.java
@@ -1,30 +1,95 @@
-package TCP;
+package TCPDocument;
 
 import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
 public class TCPServer {
+	private Socket client;
+	private ServerSocket serverSocket;
+	private DataInputStream dis;
+	private FileOutputStream output;
+	private boolean started = false;
 
-	public static void main(String[] args)throws Exception
-	{
-		Scanner in = new Scanner(System.in);
-		System.out.print("请输入要绑定的端口号:");
-		int number = in.nextInt();
-		ServerSocket serverSocket = new ServerSocket(number);
-		System.out.println("创建服务器成功,等待连接");
-		Socket client;
-		while(true)
-		{
+	public static void main(String[] args) {
+		new TCPServer().link();
+	}
+	
+	public void link() {
+		try {
+			serverSocket = new ServerSocket(8888);
+			System.out.println("创建服务器成功,等待连接");
 			client = serverSocket.accept();
-			DataInputStream dis = new DataInputStream(client.getInputStream());
-			String inputStr = "";
-			while (inputStr != "再见") {
-				inputStr = dis.readUTF();
-				System.out.println(inputStr);
-			}
-			client.close();
+			System.out.println("客户连接成功");
+			started = true;
+			receive();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
+	
+	public String Document() {
+		Scanner in = new Scanner(System.in);
+		System.out.print("请输入文件路径:");
+		String pathName = in.nextLine();
+		return pathName;
+	}
+
+	public void receive() {
+		String pathName = Document();
+		File file = new File(pathName);
+
+		try {
+			dis = new DataInputStream(client.getInputStream());
+			
+			if (!file.exists()) {
+				
+				if (file.createNewFile()) {
+					output = new FileOutputStream(file);
+						while (started) {
+							byte[] data = new byte[1024];
+							String str = dis.readUTF();	
+							data = str.getBytes();
+							output.write(data);
+							//System.out.println(str);
+						}
+				}
+			} else {
+				System.out.println("复制路径已存在相同文件名");
+				System.out.println("传输失败");
+			}
+			System.out.println("传输完毕");
+		} catch (Exception e) {
+			//e.printStackTrace();
+		} finally {
+			disconnect();
+		}
+
+	}
+
+	public void disconnect() {
+		try {
+			if (output != null) {
+				output.close();
+			}
+			if(dis != null)
+			{
+				dis.close();
+			}
+			if(client != null)
+			{
+				client.close();
+			}
+			if(serverSocket != null)
+			{
+				serverSocket.close();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
 }
